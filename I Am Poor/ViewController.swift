caafeeaa07258160e6d0d80fd860057cd196f7ba
Fried-wogns20 <84609921+wogns-kim@users.noreply.github.com>
2024-07-10 19:37:43 +0900

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 김재훈 on 7/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
